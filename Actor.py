9fe93686e53d04b4b7a81c2fd0ffd3c1b68c9fc2
Edward James <edwardjames1025@gmail.com>
2022-05-04 16:30:40 -0400

    update outline

--- Actor.py
+++ Actor.py
@@ -1,4 +1,10 @@
+import pandas as pd
+
+moviesdf = pd.read_csv("movies.csv")
+creditsdf =  pd.read_csv("credits.csv")
+
 class Actor:
+    
     def __init__(self,release_date,genre,actor_name, movie_name):
         """Initalize release date, genre, actor_name, movie_name
     Args:
@@ -7,7 +13,10 @@ class Actor:
         actor_name(str)- name of actor in movie
         movie_name(str)- name of movie
         """
-  
+        self.release_date = release_date
+        self.genre = genre
+        self.actor_name = actor_name
+        self.movie_name = movie_name
   
     def get_genre(self,movie_name):
         """return the genre of the movie based on the move name from database
@@ -16,9 +25,8 @@ class Actor:
    Returns:
        str- genre of movie
     """
-        pass
-
-    def release_date(self,movie_name):
+    
+    def get_releasedate(self,movie_name):
         """return the release date of the movie based on the move name from csv file
    Args:
        movie_name(str)- the name of the movie
@@ -63,7 +71,7 @@ class Actor:
     
 class Trivia:
     
-    def __innit__(self,question,answer,response):
+    def __init__(self,question,answer,response):
         """Initialize question and answer and response parameters
 
         Args:
@@ -94,7 +102,9 @@ class Trivia:
             
         Returns: user response to trivia question
         """
+        response = input("Enter response:")
     
+        
     def validate_response(self, response, answer):
         """validate that users response to question is correct
 
