914eae601a4496d9b78c372a256d012df530fd9a
Edward James <edwardjames1025@gmail.com>
2022-05-05 01:28:02 -0400

    worked on movie list and info method

--- Actor.py
+++ Actor.py
@@ -1,10 +1,4 @@
-import pandas as pd
-
-moviesdf = pd.read_csv("movies.csv")
-creditsdf =  pd.read_csv("credits.csv")
-
 class Actor:
-    
     def __init__(self,release_date,genre,actor_name, movie_name):
         """Initalize release date, genre, actor_name, movie_name
     Args:
@@ -13,10 +7,7 @@ class Actor:
         actor_name(str)- name of actor in movie
         movie_name(str)- name of movie
         """
-        self.release_date = release_date
-        self.genre = genre
-        self.actor_name = actor_name
-        self.movie_name = movie_name
+  
   
     def get_genre(self,movie_name):
         """return the genre of the movie based on the move name from database
@@ -25,13 +16,8 @@ class Actor:
    Returns:
        str- genre of movie
     """
-<<<<<<< HEAD
     
-    def get_releasedate(self,movie_name):
-=======
-
     def release_date(self,movie_name):
->>>>>>> 7afae73ec5a293d26454a3d0dfc26559f2f48a79
         """return the release date of the movie based on the move name from csv file
    Args:
        movie_name(str)- the name of the movie
@@ -73,9 +59,8 @@ class Actor:
     
 class Trivia:
     
-    def __init__(self,question,answer,response):
+    def __innit__(self,question,answer,response):
         """Initialize question and answer and response parameters
-
         Args:
             question (str): trivia question that is being asked
             answer (str): answer to trivia question
@@ -83,27 +68,21 @@ class Trivia:
         """
     def ask_question(self,question):
         """ask user a question based on movie/actor
-
         Args:
             question (str): question based on movie/actor
             
         Return: (str):question that user must answer
-
         """
     def user_response(self, response):
         """store users response to question
-
         Args:
             response (str): user response to trivia question
             
         Returns: user response to trivia question
         """
-        response = input("Enter response:")
     
-        
     def validate_response(self, response, answer):
         """validate that users response to question is correct
-
         Args:
             response (str): user response
             answer (str): answer to question
